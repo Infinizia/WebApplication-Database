bbff7501abd765cc246172b71304570af0721be1
Kevin <hnguyen2493@yahoo.com>
2017-05-19 23:25:49 -0700

    Upload

---
+++ CS122B/src/ViewModel/reCaptchaConfig.java
@@ -0,0 +1,6 @@
+package ViewModel;
+
+public class reCaptchaConfig {
+	public static final String SITE_KEY ="6Le3giEUAAAAAL4cftXr5i2FFhUQIuxIICpHxLV9";
+ 	public static final String SECRET_KEY ="6Le3giEUAAAAANY23hBMg2Uqx4scN26GdGnEmi6S";
+}
