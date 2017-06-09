8b2d54a56e35db967e4e29ed5f80b1e1886ddcf1
Kevin <hnguyen2493@yahoo.com>
2017-06-09 03:34:39 -0700

    Reupload

---
+++ CS122B/src/DbContext/dbFullText.java
@@ -0,0 +1,42 @@
+package DbContext;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import DbModel.MovieTitleSearch;
+
+
+public class dbFullText extends dbContext {
+	public static final String entry_col = "entry";
+	public static final String movieId_col = "movieID";
+
+	public dbFullText(String setting){
+		super(setting);
+		this.tableName = "employees";
+	}
+	
+	public ArrayList<MovieTitleSearch> GetMovie(String searchTextQuery){
+
+		try{
+			String selectQuery = String.format("select * from ft where match(entry) against('%s' in boolean mode)", searchTextQuery);
+			PreparedStatement ps = sqlConnection.prepareStatement(selectQuery);
+			ResultSet r = ps.executeQuery();
+			ArrayList<MovieTitleSearch> movieList = new ArrayList<MovieTitleSearch>();
+			
+		    while (r.next())
+		    {
+		    	MovieTitleSearch movie = new MovieTitleSearch();
+		    	movie.movieId = r.getInt(movieId_col);
+		    	movie.title = r.getString(entry_col);
+		    	movieList.add(movie);
+		    }
+			return movieList;
+		}
+		catch(Exception e){
+			System.out.println("Error occured, credit card does not exist");
+			return null;
+		}
+	}
+
+}
