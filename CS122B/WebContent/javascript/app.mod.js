2a269d278e96dda1a245e9bfdd4fe188f5032a9e
Kevin <hnguyen2493@yahoo.com>
2017-06-09 03:32:55 -0700

    Remove files

--- CS122B/WebContent/javascript/app.mod.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	'use strict';
-	angular.module('fabflixApp', ['ngRoute', 'ngResource', 'ui-notification'])
-		.config(function(NotificationProvider) {
-			NotificationProvider.setOptions({
-				startTop: 80
-			})
-		});
-})();
